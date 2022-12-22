87cc70f093930fdb9e689ce5d81007c388a90eac
kxpler442b <benmoss2002@fastmail.co.uk>
2022-12-22 23:15:23 +0000

    first commit

---
+++ src/auth.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Author: B Moss
+ * Email: benmoss2002@fastmail.co.uk
+ * Date: 22/12/22
+ * 
+ * @author B Moss
+ */
+
+declare (strict_types = 1);
+
+namespace Phpocketbase;
+
+class Auth
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function __destruct()
+    {
+        
+    }
+
+    
+}
